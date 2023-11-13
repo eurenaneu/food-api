4908626c036e1d6b05c89bfcb7276b5b6e152ffb
Renan Machado <renanpando@gmail.com>
2023-11-13 17:14:10 -0300

    atualizado: boolean adicionado ao DTO

--- src/main/java/com/example/food/dto/FoodRequestDTO.java
+++ src/main/java/com/example/food/dto/FoodRequestDTO.java
@@ -2,5 +2,6 @@ package com.example.food.dto;
 
 public record FoodRequestDTO(Integer id,
                              String nome,
-                             Double valor) {
+                             Double valor,
+                             Boolean active) {
 }
